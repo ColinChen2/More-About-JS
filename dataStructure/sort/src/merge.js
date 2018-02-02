7ca1a03f9a7e6032269df19741eb5d3bd3c8cc31
Colin Chen <colin.chen@xplusz.com>
2018-02-02 15:42:02 +0800

    #comment add recursive merge in code.

--- dataStructure/sort/src/merge.js
+++ dataStructure/sort/src/merge.js
@@ -1,4 +1,4 @@
-function merge(array) {
+function mergeBottomToTop(array) {
     if (!Array.isArray(array)) {
         return null;
     }
@@ -19,7 +19,7 @@ function merge(array) {
 
 function mergeToOne(array, start, range) {
     let start1 = start;
-    const end1 = start + range -1;
+    const end1 = start + range - 1;
 
     let start2 = start1 + range;
     const end2 = end1 + range;
@@ -48,12 +48,59 @@ function mergeToOne(array, start, range) {
         result = result.concat(array.slice(start2, end2 + 1))
     }
 
+    console.log(`result: ${result}`);
     [].splice.apply(array, [start, 2 * range].concat(result));
     console.log(`array: ${array.slice(0)}`);
 }
 
+// recursive: 1. f(n) -> f(n-1) 2. f1
+function mergeRecursive(array, start, end) {
+    // f1
+    if (start >= end) {
+        return;
+    }
+
+    const range = end - start + 1;
+    let result = [];
+
+    // fn -> fn-1
+    const mid = Math.floor((start + end) / 2);
+    let start1 = start;
+    const end1 = mid;
+    let start2 = mid + 1;
+    const end2 = end;
+
+    mergeRecursive(array, start1, end1);
+    mergeRecursive(array, start2, end2);
+
+    while (start1 <= end1 && start2 <= end2) {
+        if (array[start1] < array[start2]) {
+            result.push(array[start1++]);
+        } else {
+            result.push(array[start2++]);
+        }
+    }
+
+    if (start1 <= end1) {
+        result = result.concat(array.slice(start1, end1 + 1))
+    }
+
+    if (start2 <= end2) {
+        result = result.concat(array.slice(start2, end2 + 1))
+    }
+
+    console.log(`result: ${result}`);
+    [].splice.apply(array, [start, range].concat(result));
+}
+
+function mergeTopToBottom(array) {
+    mergeRecursive(array, 0, array.length - 1);
+    return array;
+}
+
 function run(array) {
-    console.log(merge(array));
+    // console.log(mergeBottomToTop(array));
+    console.log(mergeTopToBottom(array));
 }
 
 const a = [2, 3, 9, 78, 785, 545, 3, 3, 78, 5, 45];
