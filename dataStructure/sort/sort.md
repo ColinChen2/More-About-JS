c566678b9c6e56dcdbefb6ea850972c341aa09d2
Colin Chen <colin.chen@xplusz.com>
2018-02-04 17:01:07 +0800

    #comment finish radix sort.

--- dataStructure/sort/sort.md
+++ dataStructure/sort/sort.md
@@ -45,7 +45,7 @@ And then there are gems like this comment:
 2. [选择排序](doc/sort-selection.md)：选择排序（简单），堆排序
 3. [插入排序](doc/sort-insertion.md)：插入排序（简单），希尔排序
 4. [归并排序](doc/sort-merge.md)：归并排序
-5. 分布排序：基数排序
+5. [分布排序](doc/sort-distribution.md)：基数排序
 
 ### 参考链接
 1. [wiki](https://en.wikipedia.org/wiki/Sorting_algorithm)
--- dataStructure/sort/src/distribution.js
+++ dataStructure/sort/src/distribution.js
@@ -1,17 +1,3 @@
-## 分布排序
-
-### 基数排序
-到目前为止，我们都是在使用比较型的算法。总是将元素比较大小，然后再做进一步操作。
-
-而基数排序则与众不同，它是将整数按位数切割成不同的数字，然后按各个位数进行比较。当然他的元素不仅仅只是是整数，任何有特定格式的浮点数，字符串都可以。
-
-排序方式分为两种：
-1. LSD（Least significant digital），由键值的最右边开始。
-2. MSD（Most significant digital），由键值的最左边开始。
-
-这里的例子是使用LSD。
-
-```JavaScript
 function getMaxDigit(array) {
     let max = 0;
     for (let i = 0; i < array.length; i++) {
@@ -28,9 +14,9 @@ function radix(array) {
     const maxDigit = getMaxDigit(array);
     let buckets;
 
-    // min bit start
+    // min bit start (纵坐标)
     for (let i = 0; i < maxDigit; i++) {
-        // 0..9 -> 10 buckets
+        // 0..9 -> 10 buckets （横坐标）
         buckets = [];
         for (let i = 0; i < 10; i++) {
             buckets.push([]);
@@ -52,7 +38,9 @@ function radix(array) {
 
     return array
 }
-```
 
-## 源代码
-[sort-radix](../src/distribution.js)
+function run(array) {
+    console.log(radix(array));
+}
+
+const a = [2, 3, 9, 78, 785, 545, 3, 3, 78, 5, 45];
